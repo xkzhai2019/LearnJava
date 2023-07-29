064486671b1195f2fa2b67c06b1d8e66a6c93d28
xkzhai <xkzhai@Home.Win7>
2023-07-29 14:27:02 +0800

    product and consumer

--- ThreadDemo8.java
+++ ThreadDemo8.java
@@ -11,16 +11,65 @@ class Pool{
 	}
 
 	// 取元素
-	public void get(){
+	public Integer cut(){
+		if(pool.isEmpty()){
+			return new Integer(0);
+		}
 		return pool.remove(0);
 	}
 }
 
 // 生产者
 class Producer extends Thread{
+	Pool pool;
+	String name;
+
+	public Producer(String name, Pool pool){
+		this.pool = pool;
+		this.name = name;
+	}
+
+	public void run(){
+		int i = 1;
+		while(true){
+			pool.add(i);
+			//System.out.println("Producer:"+i);
+			i++;
+
+		}
+	}
 }
 
 // 消费者
 class Consumer extends Thread{
+	Pool pool;
+	String name;
+	public Consumer(String name, Pool pool){
+		this.pool = pool;
+		this.name = name;
+	}
+	public void run(){
+		while(true){
+			int tmp = pool.cut();
+			System.out.println("Consumer:"+tmp);
+			try{
+				Thread.sleep(100);
+			}
+			catch(Exception e){
+			
+			}
+		}
+	}
+}
 
+class ThreadDemo8{
+	public static void main(String[] args){
+		Pool pool = new Pool();
+		Producer p = new Producer("k",pool);
+		Consumer c = new Consumer("c",pool);
+		
+		// 启动生产与消费
+		p.start();
+		c.start();
+	}
 }
---
+++ ThreadDemo9.java
@@ -0,0 +1,93 @@
+/*
+	生产者-消费者
+*/
+
+// 生产者
+class Producer extends Thread{
+	final static int MAX = 1;
+	java.util.List<Integer> list;
+	String name;
+
+	public Producer(String name, java.util.List<Integer> list){
+		this.list = list;
+		this.name = name;
+	}
+
+	public void run(){
+		// 存放数据
+		int i = 1;
+		while(true){
+			synchronized(list){
+				int size = list.size();
+				if(size >= MAX){
+					try{
+						list.wait(10);
+					}
+					catch(Exception e){
+					}
+					
+				}
+				else{
+					list.add(new Integer(i));
+					System.out.println(name+":"+i);
+					i++;
+
+					list.notify();
+					//list.notifyAll();
+				}
+			}
+			yield();
+		}
+	}
+}
+
+// 消费者
+class Consumer extends Thread{
+	java.util.List<Integer> list;
+	String name;
+
+	public Consumer(String name, java.util.List<Integer> list){
+		this.list = list;
+		this.name = name;
+	}
+
+	public void run(){
+		// 取数据
+		while(true){
+			synchronized(list){
+				int size = list.size();
+				// 集合为空
+				if(size==0){
+					// 进入到等待队列
+					try{
+						list.wait(10);
+					}
+					catch(Exception e){
+					}
+				}
+				else{
+					Integer i = list.remove(0);
+					System.out.println(name+":"+i);
+					// 通知等待队列中的线程
+					list.notify(); // 存在死锁可能,需要与wait配合使用
+					//list.notifyAll();
+				}
+			}
+			yield();
+		}
+	}
+}
+
+class ThreadDemo9{
+	public static void main(String[] args){
+		java.util.List<Integer> list = new java.util.ArrayList<Integer>();
+		Producer p = new Producer("p",list);
+		Consumer c1 = new Consumer("c1",list);
+		Consumer c2 = new Consumer("c2",list);
+		
+		// 启动生产与消费
+		p.start();
+		c1.start();
+		c2.start();
+	}
+}
