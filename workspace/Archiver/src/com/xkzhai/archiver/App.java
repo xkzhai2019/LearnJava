22e637455b857ee2eed5a295a5d18ca2aaf49f86
xkzhai <xkzhai@Home.Win7>
2023-08-30 19:51:03 +0800

    FileInput

--- workspace/Archiver/src/com/xkzhai/archiver/App.java
+++ workspace/Archiver/src/com/xkzhai/archiver/App.java
@@ -1,5 +1,12 @@
 package com.xkzhai.archiver;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 import org.junit.Test;
 
 public class App {
@@ -42,5 +49,87 @@ public class App {
 		archiver.unarchive("unarch/3.yar", "unarch/tmp");
 		System.out.println("over");
 	}
+	
+	/**
+	 * 使用文本文件存储媒体文件
+	 * @throws Exception 
+	 */
+	@Test
+	public void readJPG() throws Exception{
+		FileOutputStream fos = new FileOutputStream("a.txt");
+		FileInputStream fis = new FileInputStream("1.jpg");
+		int b = -1;
+		while((b=fis.read())!=-1){
+			fos.write((b + "").getBytes());
+			fos.write(new byte[]{'\r','\n'});
+		}
+		fos.close();
+		fis.close();
+	}
+	
+	/**
+	 * 读取媒体文件
+	 */
+	@Test
+	public void writeJPG(){
+		
+	}
+	
+	/**
+	 * 使用文本文件存储媒体文件
+	 * @throws Exception 
+	 */
+	@Test
+	public void readJPG2() throws Exception{
+		FileWriter writer = new FileWriter("b.txt");
+		FileInputStream fis = new FileInputStream("1.jpg");
+		int len = -1;
+		byte[] buffer = new byte[1024];
+		
+		while((len=fis.read(buffer))!=-1){
+			writeByteArrToFile(buffer,0,len,writer);
+		}
+		writer.close();
+		fis.close();
+	}
+	/**
+	 * 将字节数组中的字节数写入到fos中
+	 */
+	private void writeByteArrToFile(byte[] arr, int beginIndex, int length, FileWriter writer){
+		
+		try{
+			for(int j=beginIndex;j<length;j++ ){
+				writer.write(arr[j]+"\r\n");
+			}	
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
 
+	}
+	
+	/**
+	 * 读取文本文件，恢复图像
+	 */
+	@Test
+	public void readTxt2JPG(){
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("a.txt"));
+			FileOutputStream fos = new FileOutputStream("2.jpg");
+			String line = null;
+			// 循环读行
+			while((line = reader.readLine())!=null){
+				// 把每行数字变换成byte
+				byte b = (byte)Integer.parseInt(line);
+				//fos.write(b);
+				//fos.write(new byte[]{b});
+				fos.write(Integer.parseInt(line));
+			}
+			fos.close();
+			reader.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
